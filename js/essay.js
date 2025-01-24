e1c33aef1db84bb5c48cf3e4f59a29ee249a0b6f
bamboo <2024767021@qq.com>
2025-01-24 17:14:51 +0800

    push automatically

--- js/essay.js
+++ js/essay.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const image = document.querySelector('.essay-image');
+    const images = document.querySelectorAll('.essay-image');
     const overlay = document.createElement('div');
     overlay.classList.add('overlay');
   
     // 添加遮罩层到DOM中
     document.body.appendChild(overlay);
   
-    image.addEventListener('click', function() {
-      image.classList.toggle('enlarged');
-      overlay.style.display = 'block';
-    });
+    images.forEach(function(image) {
+        image.addEventListener('click', function() {
+        image.classList.toggle('enlarged');
+        overlay.style.display = 'block';
+        });
+      });
   
     overlay.addEventListener('click', function(event) {
       if (event.target === overlay) { // 确保点击的是遮罩层本身而不是图片
