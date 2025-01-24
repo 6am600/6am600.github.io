1f84841f7f57cebf160883d097f1f7a0f935d999
bamboo <2024767021@qq.com>
2025-01-24 17:08:39 +0800

    push automatically

--- js/essay.js
+++ js/essay.js
@@ -1,20 +1,20 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const essayImage = document.querySelector('.essay-image');
-    const lightbox = document.querySelector('.lightbox');
-    const imageZoom = document.querySelector('.essay-image-zoom');
-
-    // 点击按钮时显示图片
-    essayImage.addEventListener('click', () => {
-        lightbox.style.display = 'flex';
-        imageZoom.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.essay-image');
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+  
+    // 添加遮罩层到DOM中
+    document.body.appendChild(overlay);
+  
+    image.addEventListener('click', function() {
+      image.classList.toggle('enlarged');
+      overlay.style.display = 'block';
     });
-
-    // 点击外部区域时关闭图片
-    lightbox.addEventListener('click', (event) => {
-        if (event.target === lightbox) {
-            lightbox.style.display = 'none';
-            mageZoom.style.display = 'none';
-        }
+  
+    overlay.addEventListener('click', function(event) {
+      if (event.target === overlay) { // 确保点击的是遮罩层本身而不是图片
+        image.classList.remove('enlarged');
+        overlay.style.display = 'none';
+      }
     });
-});
+  });
